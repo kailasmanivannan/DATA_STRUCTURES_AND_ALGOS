29c5d2ba6987dd0d1b5d9618ceea3156c4ee5b93
_MR.K_ <44869790+kailasmanivannan@users.noreply.github.com>
2021-05-04 13:26:22 +0530

    LEETCODE

---
+++ probs_solved/consec_ones.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0,max=0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                if(max<=count)
+                    max=count;
+            }
+            else
+                count = 0;
+        }
+        return max;
+    }
+}
