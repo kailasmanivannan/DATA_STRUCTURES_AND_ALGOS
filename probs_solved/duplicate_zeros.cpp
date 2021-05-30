96508afe718d8a2828fbd1e633f1878a2779177e
_MR.K_ <44869790+kailasmanivannan@users.noreply.github.com>
2021-05-30 21:49:25 +0530

    leetcode

---
+++ probs_solved/duplicate_zeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i =0;i<arr.size()-1;i++){
+            if(arr[i]==0){
+                //transferring
+                for(int j = arr.size()-2;j>=i;j--){
+                    arr[j+1]=arr[j];
+                }
+                i++;
+            }
+        }
+        
+    }
+};
